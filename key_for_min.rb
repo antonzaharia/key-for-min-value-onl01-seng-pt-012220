de63bc567fc8d977be75628e839706384b983d5e
ANTON ZAHARIA <zaharia_anton@yahoo.com>
2020-01-29 15:58:55 +0000

    Automatically backed up by Learn

--- key_for_min.rb
+++ key_for_min.rb
@@ -5,8 +5,20 @@ def key_for_min_value(name_hash)
   if name_hash.empty?
     return nil
   else
-  name_hash.collect do |keys, value|
-    value
+  array = []
+  name_hash.each do |keys, value|
+    array << value
   end
+  result = []
+  if array[0] < array[1] && array[0] < array[2]
+  result << array[0]
+  end
+  if array[1] < array[0] && array[1] < array[2]
+  result << array[1]
+  end
+  if array[2] < array[1] && array[2] < array[0]
+  result << array[2]
+  end
+  result
 end
 end
